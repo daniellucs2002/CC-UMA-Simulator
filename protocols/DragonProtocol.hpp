077e1826be6bb4cf55f6ee931a33de9e8853cb5a
daniellucs2002 <daniellucs2002@gmail.com>
2023-10-31 12:29:10 +0800

    finish dragon protocol

--- protocols/DragonProtocol.hpp
+++ protocols/DragonProtocol.hpp
@@ -31,41 +31,45 @@ public:
                 has_modified = true;
             }
 
-        bool has_owned = false;
+        bool has_SharedModified = false;
         for (int i = 0; i < cpunums; ++i)
             if (flags[i] && caches[i]->getState() == DragonSharedModified::getInstance()) {
-                assert(!has_owned && !has_modified);  // no modified allowed
-                has_owned = true;
+                assert(!has_SharedModified && !has_modified);  // no modified allowed
+                has_SharedModified = true;
             }
 
-        // start the logic of MOESI state transition
+        // start the logic of Dragon state transition
         if (!flags[msg.senderId]) {  // start from Invalid State
 
             // transition initiates from Invalid, which means applying changes has to be delayed
             assert(intToStringMap.find(msg.senderId) == intToStringMap.end());
 
             // on processor write miss
-            if (msg.type == WRITE_REQ) {
+            if (msg.type == WRITE_REQ) {  // BusUpdate is always associated with processor write
                 if (cnt == 0) {
                     intToStringMap.insert(std::make_pair(msg.senderId, DragonModified::getInstance()));
                 } else if (cnt == 1) {
 
-                    // invalidate the cache that holds the copy
+                    // find the cache that holds the copy and UPDATE
+                    // possibilities: DragonModified, DragonExclusive, DragonSharedClean, DragonSharedModified
                     for (int i = 0; i < cpunums; ++i)
                         if (flags[i]) {
                             cnt--;
-                            if (has_modified)
-                                assert(caches[i]->getState() == DragonModified::getInstance());
-                            if (has_owned)
-                                assert(caches[i]->getState() == DragonSharedModified::getInstance());
-                            caches[i]->setState(DragonInvalid::getInstance());
-                            caches[i]->setValid(false);
+                            if (has_modified || has_SharedModified) {
+                                assert(caches[i]->getState() == DragonModified::getInstance() ||
+                                    caches[i]->getState() == DragonSharedModified::getInstance());
+                                assert(caches[i]->getDirty());
+                                caches[i]->setDirty(false);  // to shared clean
+                            }
+                            assert(!caches[i]->getDirty());
+                            caches[i]->setState(DragonSharedClean::getInstance());
                         }
                     assert(cnt == 0);
-                    intToStringMap.insert(std::make_pair(msg.senderId, DragonModified::getInstance()));
+                    intToStringMap.insert(std::make_pair(msg.senderId, DragonSharedModified::getInstance()));
 
                     // sending a cache block with N words to another cache takes 2N cycles
-                    return CacheConfig::blocksize / 2;
+                    // BusUpdate takes only two cycles; first send the whole block, then bus update
+                    return CacheConfig::blocksize / 2 + TimeConfig::BusUpdate;
                 } else {
                     assert(!has_modified);  // Modified cannot exist
                     for (int i = 0; i < cpunums; ++i)
@@ -75,14 +79,18 @@ public:
                                 caches[i]->getState() == DragonSharedModified::getInstance());
                             if (caches[i]->getState() == DragonSharedClean::getInstance())
                                 assert(!caches[i]->getDirty());
-                            caches[i]->setState(DragonInvalid::getInstance());
-                            caches[i]->setValid(false);
+                            caches[i]->setState(DragonSharedClean::getInstance());
+                            if (caches[i]->getState() == DragonSharedModified::getInstance()) {
+                                assert(caches[i]->getDirty());
+                                caches[i]->setDirty(false);  // to shared clean
+                            }
+                            assert(!caches[i]->getDirty());
                         }
                     assert(cnt == 0);
-                    intToStringMap.insert(std::make_pair(msg.senderId, DragonModified::getInstance()));
-                    return CacheConfig::blocksize / 2;
+                    intToStringMap.insert(std::make_pair(msg.senderId, DragonSharedModified::getInstance()));
+                    return CacheConfig::blocksize / 2 + TimeConfig::BusUpdate;
                 }
-            } else {  // READ_REQ, which can reveal the benefits of MOESI
+            } else {  // READ_REQ
                 if (cnt == 0) {  // Invalid to Exclusive, state cannot be set currently
                     intToStringMap.insert(std::make_pair(msg.senderId, DragonExclusive::getInstance()));
                 } else if (cnt == 1) {
@@ -95,7 +103,7 @@ public:
                             }
                         intToStringMap.insert(std::make_pair(msg.senderId, DragonSharedClean::getInstance()));
                         return CacheConfig::blocksize / 2;  // improvement: no writing back in this case
-                    } else if (has_owned) {  // O
+                    } else if (has_SharedModified) {  // O
                         intToStringMap.insert(std::make_pair(msg.senderId, DragonSharedClean::getInstance()));
                         return CacheConfig::blocksize / 2;
                     } else {  // E and S
@@ -135,34 +143,40 @@ public:
                 assert(cnt == 1);
                 caches[msg.senderId]->setState(DragonModified::getInstance());
             } else if (caches[msg.senderId]->getState() == DragonSharedClean::getInstance()) {
-                assert(!caches[msg.senderId]->getDirty());
-                caches[msg.senderId]->setState(DragonModified::getInstance());
-                // also invalidate all the other cache lines
-                for (int i = 0; i < cpunums; ++i)
-                    if (i != msg.senderId && flags[i]) {
-                        assert(caches[i]->getState() == DragonSharedClean::getInstance() ||
-                            caches[i]->getState() == DragonSharedModified::getInstance());
-                        if (caches[i]->getState() == DragonSharedModified::getInstance())
-                            assert(has_owned && caches[i]->getDirty());
-                        else
-                            assert(!caches[i]->getDirty());
-                        caches[i]->setState(DragonInvalid::getInstance());
-                        caches[i]->setValid(false);
-                    }
-                // attention: DragonSharedModified is responsible for writing dirty block back into memory
-                // if (has_owned)
-                //     return TimeConfig::WriteBackMem;
+
+                if (cnt == 1) {  // no need to update, transfer to DragonModified
+                    assert(!caches[msg.senderId]->getDirty());
+                    caches[msg.senderId]->setState(DragonModified::getInstance());
+                } else {  // SharedClean to SharedModified, others need to be updated and transfer to SharedClean
+                    for (int i = 0; i < cpunums; ++i)
+                        if (i != msg.senderId && flags[i]) {
+                            assert(caches[i]->getState() == DragonSharedClean::getInstance() ||
+                                caches[i]->getState() == DragonSharedModified::getInstance());
+                            if (caches[i]->getState() == DragonSharedModified::getInstance()) {
+                                assert(has_SharedModified && caches[i]->getDirty());
+                                caches[i]->setDirty(false);  // to shared clean
+                                caches[i]->setState(DragonSharedClean::getInstance());
+                            } else {
+                                assert(!caches[i]->getDirty());
+                            }
+                        }
+                    caches[msg.senderId]->setState(DragonSharedModified::getInstance());
+                    assert(!caches[msg.senderId]->getDirty());
+                    return TimeConfig::BusUpdate;
+                }
             } else if (caches[msg.senderId]->getState() == DragonSharedModified::getInstance()) {
-                caches[msg.senderId]->setState(DragonModified::getInstance());
-                assert(caches[msg.senderId]->getDirty());
-                // also invalidate all the other cache lines
-                for (int i = 0; i < cpunums; ++i)
-                    if (i != msg.senderId && flags[i]) {
-                        assert(caches[i]->getState() == DragonSharedClean::getInstance() &&
-                            !caches[i]->getDirty());
-                        caches[i]->setState(DragonInvalid::getInstance());
-                        caches[i]->setValid(false);
-                    }
+
+                if (cnt == 1) {
+                    assert(caches[msg.senderId]->getDirty());
+                    caches[msg.senderId]->setState(DragonModified::getInstance());
+                } else {  // other copies are all in SharedClean state
+                    for (int i = 0; i < cpunums; ++i)
+                        if (i != msg.senderId && flags[i]) {
+                            assert(caches[i]->getState() == DragonSharedClean::getInstance());
+                            assert(!caches[i]->getDirty());
+                        }
+                    return TimeConfig::BusUpdate;
+                }
             } else if (caches[msg.senderId]->getState() == DragonModified::getInstance()) {
                 // do nothing, fall through
             } else {
